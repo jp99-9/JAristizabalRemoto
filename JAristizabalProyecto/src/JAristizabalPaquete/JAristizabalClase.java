405b47d2f847e848218af9ccbfc438d15871aaeb
horabaixa2 <horabaixa2@daw1-22>
2024-03-15 15:22:31 +0100

    La belleza es temporal

---
+++ JAristizabalProyecto/src/JAristizabalPaquete/JAristizabalClase.java
@@ -0,0 +1,8 @@
+package JAristizabalPaquete;
+
+public class JAristizabalClase {
+
+	public static void main(String[] args) {
+		System.out.println("Pablo López gran profesor, mejor persona");
+	}
+}
